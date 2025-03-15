98450da0c1c5d745ff383305ac73a42cc97d5ae0
evren <evren.erin@gmail.com>
2025-03-16 02:44:03 +0300

    carter ve router eklendi

--- eshop-microservice/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservice/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
 
 var app = builder.Build();
 
-
+app.MapCarter();
 app.Run();
