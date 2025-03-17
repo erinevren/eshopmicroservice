5f12a8cbdbec98454e024b21b8ef4cd134a17269
evren <evren.erin@gmail.com>
2025-03-17 03:49:14 +0300

    Marten kütüphanesi eklendi, docker compose hazırlandı

--- eshop-microservice/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservice/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,20 @@
 ﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price):ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductHandler(IDocumentSession documentSession) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product { Name= request.Name, Categories= request.Categories, Description= request.Description, ImageFile=  request.ImageFile, Price= request.Price };
+            documentSession.Store(product);
+            await documentSession.SaveChangesAsync();
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- eshop-microservice/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservice/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,15 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+
+builder.Services.AddMarten(opt => {
+    builder.Configuration.GetConnectionString("Database");
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
